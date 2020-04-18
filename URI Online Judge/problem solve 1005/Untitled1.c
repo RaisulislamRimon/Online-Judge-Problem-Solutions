03c01e4afd784f6b075d2ee4a6cb6298fc81985a
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2020-04-19 00:41:11 +0600

    Update Untitled1.c

--- URI Online Judge/problem solve 1005/Untitled1.c
+++ URI Online Judge/problem solve 1005/Untitled1.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    double A,B,MEDIA;
-    scanf("%lf%lf",&A,&B);
-    MEDIA=A/B;
+    double X, Y, A, B, MEDIA;
+
+    X = 3.5;
+    Y = 7.5;
+
+    scanf("%lf",&A);
+    scanf("%lf",&B);
+
+    MEDIA = ((A*X)+(B*Y))/(X+Y);
+
     printf("MEDIA = %.5lf\n",MEDIA);
+
     return 0;
 }
