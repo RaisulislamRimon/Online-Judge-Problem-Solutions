489caf480b0640b41a03c07ce99eb6e741ba28db
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2021-02-22 11:38:13 +0600

    Update 1000.cpp

--- POJ (PEKING UNIVERSITY Judge Online for ACM-ICPC)/1000.cpp
+++ POJ (PEKING UNIVERSITY Judge Online for ACM-ICPC)/1000.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int main()
 {
     long long num1, num2;
+    
+    // We can also use this: "int num1, num2;" this is 100% accepted, i have tested it with judge and accepted.
 
     cin >> num1 >> num2;
 
